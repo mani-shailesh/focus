418026d4a4e709972c985f359fdc96a3abe88402
mani-shailesh <shailesh.pandey@iitrpr.ac.in>
2017-03-16 17:42:00 +0530

    Reference models using String names to avoid circular dependencies

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,6 +8,12 @@ class User(AbstractUser):
     pass
 
 
+class Club(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField()
+    requests = models.ManyToManyField('User', through='ClubMembershipRequest')
+
+
 class ClubMembershipRequest(models.Model):
     STATUS_CHOICES = (
         ('PD', 'Pending'),
@@ -15,30 +21,23 @@ class ClubMembershipRequest(models.Model):
         ('RE', 'Rejected'),
         ('CN', 'Cancelled'),
     )
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
-    club = models.ForeignKey(Club, on_delete=models.CASCADE, blank=False)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, blank=False)
+    club = models.ForeignKey('Club', on_delete=models.CASCADE, blank=False)
     initiated = models.DateTimeField(auto_now_add=True, blank=False)
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, blank=False, default='PD')
     closed = models.DateTimeField(default=None)
 
 
-class Club(models.Model):
-    name = models.CharField(max_length=100, blank=False)
-    description = models.TextField()
-    requests = models.ManyToManyField(User, through=ClubMembershipRequest)
-
-
-
 class ClubRole(models.Model):
     name = models.CharField(max_length=100, blank=False)
     description = models.TextField()
-    club = models.ForeignKey(Club, on_delete=models.CASCADE, blank=False)
-    members = models.ManyToManyField(User, through=ClubMembership)
+    club = models.ForeignKey('Club', on_delete=models.CASCADE, blank=False)
+    members = models.ManyToManyField('User', through='ClubMembership')
 
 
 class ClubMembership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
-    club_role = models.ForeignKey(ClubRole, on_delete=models.CASCADE, blank=False)
+    club_role = models.ForeignKey('ClubRole', on_delete=models.CASCADE, blank=False)
     joined = models.DateTimeField(auto_now_add=True, blank=False)
 
 
@@ -47,51 +46,51 @@ class Project(models.Model):
     description = models.TextField()
     started = models.DateTimeField(auto_now_add=True, blank=False)
     closed = models.DateTimeField(default=None)
-    leader = models.ForeignKey(User, on_delete=models.PROTECT, blank=False)
-    members = models.ManyToManyField(User, through=ProjectMembership)
+    leader = models.ForeignKey('User', on_delete=models.PROTECT, blank=False, related_name='lead_projects')
+    members = models.ManyToManyField('User', through='ProjectMembership')
 
 
 class ProjectMembership(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=False)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, blank=False)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE, blank=False)
     joined = models.DateTimeField(auto_now_add=True, blank=False)
 
 
 class Channel(models.Model):
     name = models.CharField(max_length=100, blank=False)
     description = models.TextField()
-    club = models.OneToOneField(Club, on_delete=models.CASCADE, blank=False)
-    subscribers = models.ManyToManyField(User, through=ChannelSubscription)
+    club = models.OneToOneField('Club', on_delete=models.CASCADE, blank=False)
+    subscribers = models.ManyToManyField('User', through='ChannelSubscription')
 
 
 class ChannelSubscription(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
-    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, blank=False)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, blank=False)
+    channel = models.ForeignKey('Channel', on_delete=models.CASCADE, blank=False)
     joined = models.DateTimeField(auto_now_add=True, blank=False)
 
 
 class Post(models.Model):
     content = models.TextField(blank=False)
     created = models.DateTimeField(auto_now_add=True, blank=False)
-    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, blank=False)
+    channel = models.ForeignKey('Channel', on_delete=models.CASCADE, blank=False)
 
 
 class Conversation(models.Model):
     content = models.TextField(blank=False)
     created = models.DateTimeField(auto_now_add=True, blank=False)
-    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, blank=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
-    parent = models.ForeignKey("Conversation", on_delete=models.CASCADE, default=None)
+    channel = models.ForeignKey('Channel', on_delete=models.CASCADE, blank=False)
+    author = models.ForeignKey('User', on_delete=models.CASCADE, blank=False)
+    parent = models.ForeignKey('Conversation', on_delete=models.CASCADE, default=None)
 
 
 class Feedback(models.Model):
     content = models.TextField(blank=False)
     created = models.DateTimeField(auto_now_add=True, blank=False)
-    club = models.ForeignKey(Club, on_delete=models.CASCADE, blank=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    club = models.ForeignKey('Club', on_delete=models.CASCADE, blank=False)
+    author = models.ForeignKey('User', on_delete=models.CASCADE, blank=False)
 
 
 class FeedbackReply(models.Model):
     content = models.TextField(blank=False)
     created = models.DateTimeField(auto_now_add=True, blank=False)
-    parent = models.OneToOneField(Feedback, on_delete=models.CASCADE, blank=False)
+    parent = models.OneToOneField('Feedback', on_delete=models.CASCADE, blank=False)
