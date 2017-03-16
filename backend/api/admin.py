992ce80300481a9006e8e29f09cbeaba19603d1f
mani-shailesh <shailesh.pandey@iitrpr.ac.in>
2017-03-16 17:58:10 +0530

    Register main models with Django admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.User)
+admin.site.register(models.Club)
+admin.site.register(models.Project)
+admin.site.register(models.Feedback)
+admin.site.register(models.FeedbackReply)
+admin.site.register(models.Channel)
+admin.site.register(models.Post)
+admin.site.register(models.Conversation)
+admin.site.register(models.ClubRole)
