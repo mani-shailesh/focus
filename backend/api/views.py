5990c279d2a1446289f6cde58ad93b1c14f4e88a
mani-shailesh <shailesh.pandey@iitrpr.ac.in>
2017-04-01 23:09:05 +0530

    Add docstrings for views

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,11 +5,17 @@ from rest_framework import filters as rest_filters
 
 
 class UserList(generics.ListCreateAPIView):
+    """
+    View to return the list of users
+    """
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
 
 
 class UserDetail(generics.RetrieveUpdateAPIView):
+    """
+    View to allow retrieval and updation of the details of a user 
+    """
     queryset = models.User.objects.all()
     permission_classes = (rest_permissions.IsAuthenticated,
                           permissions.IsSelfOrReadOnlyUser)
@@ -17,6 +23,9 @@ class UserDetail(generics.RetrieveUpdateAPIView):
 
 
 class ClubList(generics.ListCreateAPIView):
+    """
+    View to return the list of clubs as specified by query parameters and to create clubs
+    """
     queryset = models.Club.objects.all()
     serializer_class = serializers.ClubSerializer
     permission_classes = (rest_permissions.IsAuthenticated,
@@ -27,6 +36,9 @@ class ClubList(generics.ListCreateAPIView):
 
 
 class ClubDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View to allow retrieval updation and deletion of the details of a club 
+    """
     queryset = models.Club.objects.all()
     permission_classes = (rest_permissions.IsAuthenticated,
                           permissions.IsSecyOrRepOrReadOnlyClub)
@@ -34,12 +46,18 @@ class ClubDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class ClubRoleList(generics.ListCreateAPIView):
+    """
+    View to return the list of club roles as specified by query parameters and to create club roles
+    """
     queryset = models.ClubRole.objects.all()
     serializer_class = serializers.ClubRoleSerializer
     filter_backends = (filters.MyClubRolesFilterBackend,)
 
 
 class ClubRoleDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View to allow retrieval updation and deletion of the details of a club role based on the appropriate permissions 
+    """
     queryset = models.ClubRole.objects.all()
     permission_classes = (rest_permissions.IsAuthenticated,
                           permissions.IsRepClubRole)
@@ -47,12 +65,18 @@ class ClubRoleDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class ClubMembershipList(generics.ListAPIView):
+    """
+    View to return the list of club role memberships as specified by query parameters
+    """
     queryset = models.ClubMembership.objects.all()
     serializer_class = serializers.ClubMembershipSerializer
     filter_backends = (filters.MyClubMembershipsFilterBackend,)
 
 
 class ClubMembershipDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View to allow retrieval updation and deletion of a club role membership based on appropriate permissions 
+    """
     queryset = models.ClubMembership.objects.all()
     permission_classes = (rest_permissions.IsAuthenticated,
                           permissions.IsRepClubMembership)
@@ -60,16 +84,25 @@ class ClubMembershipDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class ChannelList(generics.ListAPIView):
+    """
+    View to return the list of channels as specified by query parameters
+    """
     queryset = models.Channel.objects.all()
     serializer_class = serializers.ChannelSerializer
 
 
 class ChannelDetail(generics.RetrieveUpdateAPIView):
+    """
+    View to allow retrieval and updation of a channel based on appropriate permissions 
+    """
     queryset = models.Channel.objects.all()
     serializer_class = serializers.ChannelSerializer
 
 
 class PostList(generics.ListCreateAPIView):
+    """
+    View to return the list of posts as specified by query parameters and to create posts
+    """
     queryset = models.Post.objects.all()
     serializer_class = serializers.PostSerializer
     filter_backends = (rest_filters.SearchFilter,
@@ -78,6 +111,9 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View to allow retrieval, updation and deletion of a channel based on appropriate permissions 
+    """
     queryset = models.Post.objects.all()
     permission_classes = (rest_permissions.IsAuthenticated,
                           permissions.IsRepOrReadOnlyPost)
@@ -85,6 +121,9 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class ConversationList(generics.ListCreateAPIView):
+    """
+    View to return the list of conversations as specified by query parameters and to create conversations
+    """
     queryset = models.Conversation.objects.all()
     serializer_class = serializers.ConversationSerializer
     filter_backends = (rest_filters.SearchFilter,
@@ -92,10 +131,16 @@ class ConversationList(generics.ListCreateAPIView):
     search_fields = ('content',)
 
     def perform_create(self, serializer):
+        """
+        Override create to make sure that current user is automatically registered as the author 
+        """
         serializer.save(author=self.request.user)
 
 
 class ConversationDetail(generics.RetrieveAPIView):
+    """
+    View to allow retrieval of a conversation based on appropriate permissions 
+    """
     queryset = models.Conversation.objects.all()
     permission_classes = (rest_permissions.IsAuthenticated,
                           permissions.IsClubMemberReadOnlyConversation)
@@ -103,12 +148,18 @@ class ConversationDetail(generics.RetrieveAPIView):
 
 
 class ProjectList(generics.ListCreateAPIView):
+    """
+    View to return the list of projects as specified by query parameters and to create projects
+    """
     queryset = models.Project.objects.all()
     serializer_class = serializers.ProjectSerializer
     filter_backends = (filters.MyProjectsFilterBackend,)
 
 
 class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View to allow retrieval, updation and deletion of a project based on appropriate permissions 
+    """
     queryset = models.Project.objects.all()
     serializer_class = serializers.ProjectDetailSerializer
     permission_classes = (rest_permissions.IsAuthenticated,
@@ -116,12 +167,18 @@ class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class FeedbackList(generics.ListCreateAPIView):
+    """
+    View to return the list of feedbacks as specified by query parameters and to create feedbacks
+    """
     queryset = models.Feedback.objects.all()
     serializer_class = serializers.FeedbackSerializer
     filter_backends = (filters.MyClubFeedbacksFilterBackend,)
 
 
 class FeedbackDetail(generics.RetrieveAPIView):
+    """
+    View to allow retrieval of a feedback details based on appropriate permissions 
+    """
     queryset = models.Feedback.objects.all()
     serializer_class = serializers.FeedbackDetailSerializer
     permission_classes = (rest_permissions.IsAuthenticated,
@@ -129,10 +186,16 @@ class FeedbackDetail(generics.RetrieveAPIView):
 
 
 class FeedbackReplyCreate(generics.CreateAPIView):
+    """
+    View to create feedback replies
+    """
     serializer_class = serializers.FeedbackReplySerializer
 
 
 class FeedbackReplyDetail(generics.RetrieveAPIView):
+    """
+    View to allow retrieval of a feedback reply details based on appropriate permissions 
+    """
     queryset = models.FeedbackReply.objects.all()
     serializer_class = serializers.FeedbackReplyDetailSerializer
     permission_classes = (rest_permissions.IsAuthenticated,
