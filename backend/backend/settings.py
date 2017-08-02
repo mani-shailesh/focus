37531ff1205a2c6a8a984995816fc8c16bfd2a55
mani-shailesh <shailesh.pandey@iitrpr.ac.in>
2017-08-02 21:03:04 +0530

    Enable pagination for the response

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,5 +137,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
-    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 50,
 }
