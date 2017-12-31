1329cd7d8ee9233c7765e9a62da8089d40d0554e
mani-shailesh <shailesh.pandey@iitrpr.ac.in>
2017-12-31 22:06:37 +0530

    Add TokenAuthentication scheme

--- focus/settings.py
+++ focus/settings.py
@@ -170,6 +170,10 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50,
